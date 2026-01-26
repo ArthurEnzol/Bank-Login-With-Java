182a9d289d4cf937e5b08b6edcc13e91c5191266
ArthurEnzol <arthursantosfernandes337@gmail.com>
2026-01-26 17:15:54 -0300

    refactor: otimize logic and exclude methods

--- BankLogin/src/entities/AccountBank.java
+++ BankLogin/src/entities/AccountBank.java
@@ -1,13 +1,17 @@
 package entities;
 
+import entities.enums.StatusErrors;
+
 import java.lang.Math;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AccountBank {
 
     private int numberAccount;
     private String nameTitleAccount;
     private double sale;
+
     public static ArrayList<Integer> listNumbersAccounts = new ArrayList<>();
     public static ArrayList<String> listNamesAccounts = new ArrayList<>();
 
@@ -15,28 +19,11 @@ public class AccountBank {
         this.nameTitleAccount = nameTitleAccount;
     }
 
-    public int getNumberAccount() {
-        return numberAccount;
-    }
-
     public String getNameTitleAccount() {
         return nameTitleAccount;
     }
 
-    public void setNameTitleAccount(String nameTitleAccount) {
-        this.nameTitleAccount = nameTitleAccount;
+    public int getNumberAccount() {
+        return numberAccount;
     }
-
-    public double getSale() {
-        return sale;
-    }
-
-    public void setSale(double sale) {
-        this.sale = sale;
-    }
-
-
-
-
-
 }
